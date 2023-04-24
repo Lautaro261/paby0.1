c3ed4aa72f4de12d8378d750e9c6314ed05bc658
Andres Garcia <andresfgarciac96@gmail.com>
2023-04-24 16:25:54 -0500

    loading finish

--- src/loading/Loading.jsx
+++ src/loading/Loading.jsx
@@ -4,6 +4,7 @@ export default function Loading() {
   return (
     <div className="loading-container">
       <h2>Loading...</h2>
+      <h4>Cargando...</h4>
       <button>Enviar</button>
     </div>
   );
