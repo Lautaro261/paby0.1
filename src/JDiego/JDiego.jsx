0761260b4d30b31a6c53fe01e9b8283d325876bd
j10gof <juandiegoflorezlora@gmail.com>
2023-04-24 17:06:16 -0500

    CSS creado para el componente

--- src/JDiego/JDiego.jsx
+++ src/JDiego/JDiego.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Style from 'JDiego.module.css';
 
 export default function JDiego() {
   return (
-      <>
-      <div>Hola soy Juan Diego/div>
+      <div className={Style.texto}>
+      <h1>Hola soy Juan Diego</h1>
       <button>Enviar</button>
-      </>
-
+      </div>
   )
 }
