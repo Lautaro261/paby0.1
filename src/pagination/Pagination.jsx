4a45532329acfbf9b7179797457e9a69dc585b8b
Andres Garcia <andresfgarciac96@gmail.com>
2023-04-24 16:31:32 -0500

    pagination branch

---
+++ src/pagination/Pagination.jsx
@@ -0,0 +1,11 @@
+import "./pagination.css";
+
+export default function Loading() {
+  return (
+    <div className="pagination-container">
+      <h2>Loading...</h2>
+      <h4>Cargando...</h4>
+      <button>Enviar</button>
+    </div>
+  );
+}
